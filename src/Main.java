25c20f187369ffd8ce15bfd6897b72fc692aff63
11104 <aknietarap04@mail.ru>
2024-02-10 15:03:53 +0600

    hel

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int a= 15;
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите число N: ");

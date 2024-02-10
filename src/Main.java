1e101e98da1791a9e76ef99e339538112072db83
11104 <aknietarap04@mail.ru>
2024-02-10 14:30:17 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число N: ");
+        int n = scanner.nextInt();
+
+        // Вызываем метод для нахождения суммы чисел от 1 до N
+        int sum = calculateSum(n);
+
+        System.out.println("Сумма чисел от 1 до " + n + " равна: " + sum);
+    }
+
+    // Метод для нахождения суммы чисел от 1 до N
+    private static int calculateSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
